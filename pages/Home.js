d0d0f41752cda1fbb6d1b01e7942aec149be9395
walterunderwood <walonsounderwood@gmail.com>
2023-03-08 02:50:09 -0800

    Fetching from Firebase

--- pages/Home.js
+++ pages/Home.js
@@ -1,15 +1,38 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import { useNavigation } from '@react-navigation/core'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../config/firebase';
 
 const Home = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            let list = []
+            try {
+                const querySnapshot = await getDocs(collection(db, "users"));
+                querySnapshot.forEach((doc) => {
+                    list.push({ id: doc.id, ...doc.data() })
+                });
+                setData(list)
+                console.log(list);
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+        fetchData();
+
+    }, [])
+
+    // console.log(data);
+
 
     const navigation = useNavigation();
     return (
 
-        <Text>
-            Hello World
-        </Text>
+
+        <Button title='Add a Car' onPress={() => navigation.navigate('Add')}></Button>
     )
 }
 
