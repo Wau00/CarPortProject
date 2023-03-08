c8cb0024f8d9dca7dec496f20ea78ad8ba37c72a
walterunderwood <walonsounderwood@gmail.com>
2023-03-08 02:50:37 -0800

    Fetching from Firebase usernames

--- components/homeScreen.js
+++ components/homeScreen.js
@@ -1,10 +1,34 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { collection, getDocs } from "firebase/firestore";
+import { db } from '../config/firebase';
+
+
 
 const homeScreen = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            let list = []
+            try {
+                const querySnapshot = await getDocs(collection(db, "users"));
+                querySnapshot.forEach((doc) => {
+                    list.push(doc)
+                });
+                setData(list)
+            } catch (err) {
+                console.log(err)
+            }
+
+        }
+        fetchData();
+    }, [])
+
+    console.log(data);
+
     return (
         <View>
-            <Text>Hello There!</Text>
+            <Text>Hello There!!!</Text>
         </View>
     )
 }
