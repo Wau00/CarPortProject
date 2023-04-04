64c48dccb68041886b6d38cc03260553d67cfa50
walterunderwood <walonsounderwood@gmail.com>
2023-04-03 18:17:22 -0700

    Create garage Page

---
+++ pages/Garage.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const Garage = () => {
+
+    return (
+        <View>
+            <Text>Vehicle Details</Text>
+        </View>
+    )
+}
+
+export default Garage
