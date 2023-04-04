692d1afaf7b9d2fb6cdefa734689f6bf0b24b715
walterunderwood <walonsounderwood@gmail.com>
2023-04-03 18:17:34 -0700

    Create Profile Page

---
+++ pages/Profile.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const Profile = () => {
+
+    return (
+        <View>
+            <Text>Profile Details</Text>
+        </View>
+    )
+}
+
+export default Profile
